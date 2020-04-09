f9211309b84edeaf953c57cb87759617f7f550ec
bparikh99 <59947941+bparikh99@users.noreply.github.com>
2020-04-09 19:31:09 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,36 @@
+
+"""
+Created on Thu Apr  9 18:12:10 2020
+
+@author: himanshimehta
+"""
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+model=pickle.load(open("model.pkl","rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be INR {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', debug=True, threaded=True,use_reloader=False)
+  
+
